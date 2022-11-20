2351ece68b63da19b79962ab069e89a24be1112b
David Mabula <104975559+davidmbl-UBX@users.noreply.github.com>
2022-11-20 23:05:04 +0300

    commented GPT3

--- app.js
+++ app.js
@@ -12,29 +12,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // OpenAI GPT3 Integration
-const gpt3 = require('./gpt3')
+// const gpt3 = require('./gpt3')
 
 // warm up gpt3 library
-gpt3.runQuery().then(console.log)
+// gpt3.runQuery().then(console.log)
 
 
 app.get('/', (req,res) => {
     res.send("Welcome to the Agrivoice REST API")
 })
 
-app.post('/query', (req,res) => {
-    let qry = req.body.query
-    gpt3.runQuery(qry).then(
-        d => {
-            res.json({success:true, message: {question:qry,answer:d}})
-        }
-    ).catch(err => {
-        if(err){
-            res.json({success:false,message:"Internet connectivity error... check that you are connected or have sufficient data"})
-        }
-    })
-    // res.send("Agrovoice query")
-})
+// app.post('/query', (req,res) => {
+//     let qry = req.body.query
+//     gpt3.runQuery(qry).then(
+//         d => {
+//             res.json({success:true, message: {question:qry,answer:d}})
+//         }
+//     ).catch(err => {
+//         if(err){
+//             res.json({success:false,message:"Internet connectivity error... check that you are connected or have sufficient data"})
+//         }
+//     })
+//     // res.send("Agrovoice query")
+// })
 
 
 app.listen(3000)
